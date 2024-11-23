6d8b67474fc930f759272d2aeb3c2ce00b3e48e0
Agena <103839428+Agena7400@users.noreply.github.com>
2024-11-23 13:26:58 +0900

    edit/PortSelectionDialog.xaml.cs

--- Views/PortSelectionDialog.xaml.cs
+++ Views/PortSelectionDialog.xaml.cs
@@ -29,12 +29,13 @@ namespace FlightController
         string[] BaudlateIndex = { "1200", "2400", "4800", "9600", "14400", "19200", "38400", "56000", "57600", "115200" };
         public PortSelectionDialog()
         {
-            InitializeComponent();//Componentを初期化
+            InitializeComponent();                  //Componentを初期化
 
-            InitializePortList();//ComboBox_PortListを初期化
-            InitializeBaudlateList();//ComboBox_Baudlateを初期化
+            InitializePortList();                   //ComboBox_PortListを初期化
+            InitializeBaudlateList();               //ComboBox_Baudlateを初期化
         }
-        private void InitializePortList() //ComboBox_PortListの初期化
+        private void InitializePortList()
+        //ComboBox_PortListの初期化
         {
             ComboBox_PortList.IsEnabled = true;
             ComboBox_PortList.ItemsSource = null;
@@ -43,24 +44,28 @@ namespace FlightController
             ComboBox_PortList.DisplayMemberPath = "Description";
             ComboBox_PortList.SelectedIndex = 0;
         }
-        private void InitializeBaudlateList()//ComboBox_Baudlateを初期化
+
+        private void InitializeBaudlateList()
+        //ComboBox_Baudlateを初期化
         {
-            ComboBox_Baudlate.Items.Clear();//ComboBox_BaudLateを初期化するために中身を削除
-            foreach (var i in BaudlateIndex)//BaudlateIndexをComboBox_Baudlateに追加する
+            ComboBox_Baudlate.Items.Clear();            //ComboBox_BaudLateを初期化するために中身を削除
+            foreach (var i in BaudlateIndex)            //BaudlateIndexをComboBox_Baudlateに追加する
             {
                 ComboBox_Baudlate.Items.Add(i);
             }
-            ComboBox_Baudlate.SelectedValue = "19200";//初期選択値を設定
+            ComboBox_Baudlate.SelectedValue = "19200";  //初期選択値を設定
         }
-        private ObservableCollection<COMPort> SerialPort_Enumeration() //シリアルポートをObservableCollectionに列挙して返す
+
+        private ObservableCollection<COMPort> SerialPort_Enumeration()
+        //シリアルポートをObservableCollectionに列挙して返す
         {
             var SerialPortList = new ObservableCollection<COMPort>();
 
             var check = new System.Text.RegularExpressions.Regex("(COM[0-9]{1,3})");
 
-            foreach(ManagementObject m in new ManagementClass("Win32_PnPEntity").GetInstances())//全てのPnPデバイスを調べ、シリアル通信を行うデバイスを追加
+            foreach(ManagementObject m in new ManagementClass("Win32_PnPEntity").GetInstances())    //全てのPnPデバイスを調べ、シリアル通信を行うデバイスを追加
             {
-                string name = m.GetPropertyValue("name")?.ToString() ?? string.Empty; //null条件演算子・null合体演算子
+                string name = m.GetPropertyValue("name")?.ToString() ?? string.Empty;               //null条件演算子・null合体演算子
 
                 if (check.IsMatch(name))//nameプロパティがcheckで指定した正規表現に部分一致した場合リストに追加
                 {
